08fef0b0591b340cff851a227f3227242d40a551
mmaroun <mmaroun@fadel.com>
2021-10-16 15:50:27 +0300

    init

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list-bootstrap.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  //create an array of objects 
+  salesPersonList : SalesPerson[] = [
+    new SalesPerson("Miguel","Maroun","mmaroun@gmail.com",50000),
+    new SalesPerson("John","doe","jdoe@gmail.com",66000),
+    new SalesPerson("Ali","saghir","asaghir@gmail.com",102000),
+    new SalesPerson("Rajesh","Molugu","Rmolugu@gmail.com",33000)
+  ];
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
