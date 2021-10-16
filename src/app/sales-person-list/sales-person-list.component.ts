77fe6a54f55df241e71b012fd5d5cbda76724220
mmaroun <mmaroun@fadel.com>
2021-10-16 16:13:29 +0300

    test-commit

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -11,7 +11,7 @@ export class SalesPersonListComponent implements OnInit {
   //create an array of objects 
   salesPersonList : SalesPerson[] = [
     new SalesPerson("Miguel","Maroun","mmaroun@gmail.com",50000),
-    new SalesPerson("John","doe","jdoe@gmail.com",66000),
+    new SalesPerson("John","doe","jdoe@gmail.com",67000),
     new SalesPerson("Ali","saghir","asaghir@gmail.com",102000),
     new SalesPerson("Rajesh","Molugu","Rmolugu@gmail.com",33000)
   ];
